a26e389eefc127baffb09d6f3a058a40c5d5ce0a
kevin schuchard <schuchard.kevin@gmail.com>
2019-11-07 22:09:54 -0700

    remove build files

--- packages/schematic/interfaces/ts-config-schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * tsconfig.json schema: http://json.schemastore.org/tsconfig
- * Generated with https://transform.now.sh/json-schema-to-ts/
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ts-config-schema.js.map
--- packages/schematic/jest/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Rule } from '@angular-devkit/schematics';
-import { JestOptions } from '../utility/util';
-export default function (options: JestOptions): Rule;
